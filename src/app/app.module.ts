9006b92089fa21927eac71a02728c3e71600e1e5
IgorDA <igordiasbr@gmail.com>
2018-03-21 21:08:21 -0300

    Angular aula 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Pagina1Component } from './pagina1/pagina1.component';
+import { Pagina2Component } from './pagina2/pagina2.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { Pagina3Component } from './pagina3/pagina3.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina1Component,
+    Pagina2Component,
+    HeaderComponent,
+    Pagina3Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
